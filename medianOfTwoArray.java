3642d4bee6278e34fc001794570415da73e5e8f7
hackster2099 <faresshiraliEng@gmail.com>
2026-01-02 21:16:52 -0800

    defined and modified some of the methods for the operation

--- medianOfTwoArray.java
+++ medianOfTwoArray.java
@@ -1,29 +1,52 @@
 class Solution {
 
-    int sumOfAllData1; // for the first array
-    int sumOfAllData2; // for the second array
+
     int missingDataSum1;
     int missingDataSum2;
-    int maxVal;
     int missingDataNum;
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-
-
+        
     }
 
-    public static int sumFunc(){}
+    public static int sumFunc(int maxValue){
+
+        int sumOfAllData = 0;
+
+        for(int i=0; i <= maxValue; i++){ 
+
+            sumOfAllData += i;
+
+        }
+
+        return sumOfAllData;
+        
+    }
    
     public static int maxValFinder(int[] nums1, int[] nums2){
 
+        int maxVal;
+
         if(nums1[nums1.length - 1] > nums2[nums2.length - 1] ) {maxVal = nums1[nums1.length - 1]}    
         else if( nums2[nums2.length - 1] > nums1[nums1.length - 1] ) {maxVal = nums2[nums1.length - 1]}    
-        else{maxVal = nums1[nums1.length - 1]}   
+        else{maxVal = nums1[nums1.length - 1]}  
+
+        return maxVal; 
 
     }
     
-    public static int missingDataNum(){}
+
+    public static int missingDataNum(int[] nums1, int[] nums2){
+
+        for(int i=0; i<nums1.lenght; i++){
+
+            
+
+        }
+
+    }
+
     public static int missingSumFunc(){}
 
 
-}
+} 
