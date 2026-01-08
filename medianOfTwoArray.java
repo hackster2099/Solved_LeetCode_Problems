651a6beb6e4d1428852c8e60eddb4344144c235a
hackster2099 <faresshiraliEng@gmail.com>
2026-01-07 16:32:43 -0800

    working on the last method, which is to sum up the missing values with the time complexity of O(n)

--- medianOfTwoArray.java
+++ medianOfTwoArray.java
@@ -1,8 +1,10 @@
 class Solution {
 
-    int missingDataSum1;
-    int missingDataSum2;
-    int missingDataNum;
+    public static int minVal = 0;
+    public static int maxVal = 0;
+    public static int missingDataNum = 0;
+
+    Hashmap <int, int> foundRanges = new Hashmap<int, int>;
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
        
@@ -34,54 +36,74 @@ class Solution {
 
     }
     
-
     public static int missingDataNum(int[] nums1, int[] nums2){
 
         int difference = 0;
-        int startingValue;
-        int endingValue;
-        int numOfMissingData;
-        boolean startFlag = false;
+        boolean flag = false;
+        int tempValue;
 
-        //analyzing the first array
         for(int i=0; i<nums1.lenght; i++){
 
             difference = nums1[i+1] - nums1[i];
 
-            if((difference > 1) && !startFlag){
+            if((difference > 1) && !flag){
+
+                foundRange.put(nums1[i], nums1[i+1]);
+                missingDataNum += (difference - 1);
+                minVal = nums1[i];
+                maxVal = nums1[i+1]; 
+                flag = true;              
+
+            }
+
+            if((difference > 1) && flag){
+            
+                foundRange.put(nums1[i], nums1[i+1]);
+                missingDataNum += (difference - 1);
                 
-                startingValue = nums1[i];
-                numOfMissingData += (difference - 1);
-                startFlag = true;
-            
-            }
-
-            if((difference > 1) && startFlag && (nums1[nums1.length - 1] != nums1[i+1])){
-
-                numOfMissingData += (difference - 1);
-            
-            }
-            
-            if((difference > 1) && startFlag && (nums1[nums1.length - 1] == nums1[i+1])){
-
-                numOfMissingData += (difference - 1);
-                endingValue = nums1[i+1];
-                startFlag = false;
+                if (minVal > nums1[i]){ minVal = nums1[i];}
+                if (maxVal < nums1[i+1]){ maxVal = nums1[i+1];}                
 
             }
+
         }
 
-        for(int i=startingValue; i>= endingValue; i++){
+        for(int i=0; i<nums2.lenght; i++) {
 
-            
+            boolean flag = false;
+            int tempValue
+
+            difference = nums2[i+1] - nums2[i];
+
+            if(difference > 1) {
+
+                foundRange.put(nums2[i], nums2[i+1]);
+                missingDataNum += (difference - 1);
+                if (minVal > nums2[i]){ minVal = nums2[i];}
+                if (maxVal < nums2[i+1]){ maxVal = nums2[i+1];}   
+
+            }
+
+        }
+
+    }
+
+    public static int missingSumFunc() {
 
 
+        // implement the algorithm written on the notebook here.
+        
+
+        for(int i=minVal; i<=maxVal; i++){
+
+            if(foundRanges.containsKey(i)){
+
+
+            }    
 
         }
 
 
     }
 
-    public static int missingSumFunc(){}
-
-}  
+}   
